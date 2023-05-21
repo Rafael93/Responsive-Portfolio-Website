c45de9f27c9edd997af335a31285ff2a1ea7f06a
Rafael93 <rafaelgamas@gmail.com>
2023-05-20 21:29:13 -0400

    update 20-05-23

--- js/script.js
+++ js/script.js
@@ -1,21 +1,28 @@
 // toggle icon navbar
 
+let menuIcon = document.getElementById("menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
 // scroll sections
 
 window.onscroll = () => {
   let header = document.querySelector("header");
-
   header.classList.toggle("sticky", window.scrollY > 100);
-};
 
-let sections = document.querySelectorAll("section");
-let navLinks = document.querySelectorAll("header nav a");
+  let sections = document.querySelectorAll("section");
+  let navLinks = document.querySelectorAll("header nav a");
 
-window.onscroll = () => {
   sections.forEach((sec) => {
     let top = window.scrollY;
+
     let offset = sec.offsetTop - 100;
+
     let height = sec.offsetHeight;
+
     let id = sec.getAttribute("id");
 
     if (top >= offset && top < offset + height) {
@@ -28,4 +35,8 @@ window.onscroll = () => {
       });
     }
   });
+
+  //remove toggle icon and navbar when click navbar links  (scroll)
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
 };
